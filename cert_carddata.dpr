85954a4efffd666a5bc526c16c6b2f1c6aa216f4
Vincent Hardy <vincent.hardy.be@gmail.com>
2015-09-22 18:21:11 +0200

    Add cert_carddata example

---
+++ cert_carddata.dpr
@@ -0,0 +1,322 @@
+(* ****************************************************************************
+
+* eID Middleware Project.
+* Copyright (C) 2009-2011 FedICT.
+* Copyright (C) 2015 Vincent Hardy <vincent.hardy.be@gmail.com>
+*
+* This is free software; you can redistribute it and/or modify it
+* under the terms of the GNU Lesser General Public License version
+* 3.0 as published by the Free Software Foundation.
+*
+* This software is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this software; if not, see
+* http://www.gnu.org/licenses/.
+
+**************************************************************************** *)
+
+{$APPTYPE CONSOLE}
+program cert_carddata;
+
+//PKCS11T.pas can be found here :
+//http://sourceforge.net/p/projectjedi/website/HEAD/tree/trunk/delphi-jedi.org/www/files/API_Not_Assessed/SmartcardRSA/
+
+uses
+  {$IFDEF FPC}
+  crt,
+  {$ENDIF}
+  Windows,sysutils,
+  PKCS11T,
+  cert_registration;
+
+const
+  PKCS11DLL = 'beidpkcs11.dll';
+
+//Pascal pointers management
+type
+  CK_SLOT_IDS=array[0..255] of CK_SLOT_ID;
+  CK_SLOT_IDS_PTR=^CK_SLOT_IDS;
+  CK_BYTES = array[0..65535] of CK_BYTE;
+  CK_BYTES_PTR = ^CK_BYTES;
+
+{$IFNDEF FPC}
+//In Delphi, crt unit and Readkey function don't exist anymore
+Function ReadKey:Char;
+var
+  Buffer:TInputRecord;
+  EventRead:Cardinal;
+  stdin:Thandle;
+begin
+  stdin := GetStdHandle(STD_INPUT_HANDLE);
+  Result:=#0;
+  repeat
+    ReadConsoleInput(stdin,Buffer,1,EventRead);
+    if (EventRead=1) and
+       (Buffer.EventType=KEY_EVENT) and
+       (Buffer.Event.KeyEvent.bKeyDown) and
+       (Buffer.Event.KeyEvent.AsciiChar<>#0) then
+      Result:=Buffer.Event.KeyEvent.AsciiChar;
+  until Result<>#0;
+end;
+{$ENDIF}
+
+procedure Beidsdk_PrintValue(pName:CK_CHAR_PTR; pValue:CK_BYTE_PTR; valueLen:CK_ULONG);
+var
+  counter:longword;
+  b:CK_BYTE;
+begin
+  writeln;
+  Writeln(PAnsiChar(pName)+':');
+  Writeln;
+  if pValue<>nil then
+  begin
+    counter:=0;
+    while counter<valueLen do
+    begin
+      b:=CK_BYTES_PTR(pValue)^[counter];
+      if ($29<b) and (b<$81)
+      then
+        Write(Chr(b))
+      else
+        Write('.');
+      inc(counter);
+    end;
+  end;
+  writeln;
+end;
+
+function Beidsdk_GetObjectValue(pFunctions:CK_FUNCTION_LIST_PTR;
+                                session_handle:CK_SESSION_HANDLE;
+                                pName:CK_CHAR_PTR;
+                                ppValue:CK_VOID_PTR;
+                                pvalueLen:CK_ULONG_PTR):CK_RV;
+var
+  classType:CK_ULONG;
+  searchtemplate:array[1..2] of CK_ATTRIBUTE;
+  attrtemplate:CK_ATTRIBUTE;
+  hObject:CK_OBJECT_HANDLE;
+  ulObjectCount:CK_ULONG;
+begin
+  Result:=CKR_OK;
+  classType:=CKO_DATA;
+  With searchtemplate[1] do
+  begin
+    _type:=CKA_LABEL;
+    pValue:=pName;
+    ulValueLen:=strlen(pAnsiChar(pName));
+  end;
+  With searchtemplate[2] do
+  begin
+    _type:=CKA_CLASS;
+    pValue:=@classType;
+    ulValueLen:=sizeof(CK_ULONG);
+  end;
+  pvalueLen^:=0;
+  //initialize the search for the objects with label <filename>
+  Result:=pFunctions^.C_FindObjectsInit(session_handle, @searchtemplate, 2);
+  if (Result<>CKR_OK) then exit;
+  
+  //find the first object with label <filename>
+  Result:=pFunctions^.C_FindObjects(session_handle,@hObject,1,@ulObjectCount);
+  if ((ulObjectCount=1) and (Result=CKR_OK)) then
+  begin
+    with attrtemplate do
+    begin
+      //NULL_PTR as second argument, so the length of value is filled in to retValueLen
+      _type:=CKA_VALUE;
+      pValue:=nil;
+      ulValueLen:=0;
+    end;
+    //retrieve the length of the data from the object
+    Result:=pFunctions^.C_GetAttributeValue(session_handle,hObject,@attrtemplate,1);
+    if ((Result=CKR_OK) and  (CK_LONG(attrtemplate.ulValueLen)<>-1)) then
+    begin
+      getmem(pointer(ppValue^),attrtemplate.ulValueLen);
+      attrtemplate.pValue:=pointer(ppValue^);
+      //retrieve the data from the object
+      Result:=pFunctions^.C_GetAttributeValue(session_handle,hObject,@attrtemplate,1);
+      pvalueLen^:=attrtemplate.ulValueLen;
+    end;
+  end;
+  //finalize the search
+  Result:=pFunctions^.C_FindObjectsFinal(session_handle);
+end;
+
+function Beidsdk_RegisterCertificates(
+               pFunctions:CK_FUNCTION_LIST_PTR;
+               session_handle:CK_SESSION_HANDLE;
+               cardSerialNumber:CK_BYTE_PTR;
+               SerialNumberValueLen:CK_ULONG):CK_RV;
+var
+  searchtemplate:CK_ATTRIBUTE;
+  classType:CK_ULONG;
+  hObject:CK_OBJECT_HANDLE;
+  ulObjectCount:CK_ULONG;
+  attr_templ:CK_ATTRIBUTE;
+  pValue_:CK_BYTE_PTR;
+begin
+  pValue_:=nil;
+
+  classType:=CKO_CERTIFICATE;
+  with searchtemplate do
+  begin
+    _type:=CKA_CLASS;
+    pValue:=@classType;
+    ulValueLen:=sizeof(CK_ULONG);
+  end;
+  //initialize the search for the objects with label <filename>
+  Result:=pFunctions^.C_FindObjectsInit(session_handle, @searchtemplate, 1);
+  if (Result<>CKR_OK) then exit;
+
+  //find the first object with class CKO_CERTIFICATE
+  Result:=pFunctions^.C_FindObjects(session_handle,@hObject,1,@ulObjectCount);
+  while ((ulObjectCount=1) and (Result=CKR_OK)) do
+  begin
+    //NULL_PTR as second argument, so the length of value is filled in to retValueLen
+    with attr_templ do
+    begin
+      _type:=CKA_VALUE;
+      pValue:=nil;
+      ulValueLen:=0;
+    end;
+
+    //retrieve the length of the data from the object
+    Result:=pFunctions^.C_GetAttributeValue(session_handle,hObject,@attr_templ,1);
+    if ((Result=CKR_OK) and (CK_LONG(attr_templ.ulValueLen)<>-1)) then
+    begin
+      getmem(pValue_,attr_templ.ulValueLen);
+      if pValue_<>nil then
+      begin
+        attr_templ.pValue:=pValue_;
+        //retrieve the data from the object
+        Result:=pFunctions^.C_GetAttributeValue(session_handle,hObject,@attr_templ,1);
+        if (Result=CKR_OK)
+        then
+          ImportCertificate(pValue_,attr_templ.ulValueLen,cardSerialNumber,SerialNumberValueLen);
+        freemem(pValue_);
+      end else
+      begin
+        //error allocating memory for pValue
+        Result:=CKR_GENERAL_ERROR;
+      end;
+    end;
+    Result:=pFunctions^.C_FindObjects(session_handle,@hObject,1,@ulObjectCount);
+  end;
+  //finalize the search
+  Result:=pFunctions^.C_FindObjectsFinal(session_handle);
+end;
+
+function beidsdk_Main:CK_ULONG;
+var
+  pkcs11Handle:THandle;                       //handle to the pkcs11 library
+  pFunctions:CK_FUNCTION_LIST_PTR;            //list of the pkcs11 function pointers
+  pC_GetFunctionList:pointer;
+  slotIds:CK_SLOT_ID_PTR;
+  slot_count:CK_ULONG;
+  slotIdx:CK_ULONG;
+  session_handle:CK_SESSION_HANDLE;
+  pSerialNumber:CK_CHAR_PTR;
+  pSerialNumberValue:CK_VOID_PTR;
+  SerialNumberValueLen:CK_ULONG;
+  err:cardinal;
+begin
+  Result:=CKR_OK;
+  pSerialNumberValue:=nil;
+  //open the pkcs11 library
+  pkcs11Handle:=LoadLibrary(PKCS11DLL);
+  if pkcs11Handle>=32 then
+  begin
+    // get function pointer to C_GetFunctionList
+    pC_GetFunctionList:=GetProcAddress(pkcs11Handle,'C_GetFunctionList');
+    if pC_GetFunctionList<>nil then
+    begin
+      // invoke C_GetFunctionList to get the list of pkcs11 function pointers
+      Result:=TfC_GetFunctionList(pC_GetFunctionList)(@pFunctions);
+      if (Result=CKR_OK) then
+      begin
+        // initialize Cryptoki
+        Result:=pFunctions^.C_Initialize(nil);
+        if Result=CKR_OK then
+        begin
+          slot_count:=0;
+          // retrieve the number of slots (cardreaders) found
+          Result:=pFunctions^.C_GetSlotList(CK_TRUE,nil,@Slot_Count);
+          if (Result=CKR_OK) and (slot_count>0) then
+          begin
+            getmem(SlotIds,Slot_Count*sizeof(CK_SLOT_ID));
+            // retrieve the list of slots (cardreaders)            
+            Result:=pFunctions^.C_GetSlotList(CK_TRUE,SlotIds,@Slot_Count);
+            if (Result=CKR_OK) then
+            begin
+              for slotIdx:=0 to slot_count-1 do
+              begin
+                //open a session
+                Result:=pFunctions^.C_OpenSession(CK_SLOT_IDS_PTR(SlotIds)^[slotIdx],CKF_SERIAL_SESSION,nil,nil,@session_handle);
+                if (Result=CKR_OK) then
+                begin
+                  PAnsiChar(pSerialNumber):='carddata_serialnumber';
+                  //retrieve the data of the file
+                  Result:=Beidsdk_GetObjectValue(pFunctions,
+                                                 session_handle,
+                                                 pSerialNumber,
+                                                 @pSerialNumberValue,
+                                                 @SerialNumberValueLen);
+                  if (Result=CKR_OK) then
+                  begin
+                    Beidsdk_RegisterCertificates(pFunctions,session_handle,CK_BYTE_PTR(pSerialNumberValue),SerialNumberValueLen);
+                    Beidsdk_PrintValue(pSerialNumber,CK_BYTE_PTR(pSerialNumberValue),SerialNumberValueLen);
+                  end else
+                    Writeln(Format('error 0x%.8x Beidsdk_GetObjectValue',[Result]));
+
+                  if pSerialNumberValue<>nil then FreeMem(pSerialNumberValue);
+                end;
+                //close the session
+                if (Result=CKR_OK)
+                then
+                  Result:=pFunctions^.C_CloseSession(session_handle)
+                else
+                  pFunctions^.C_CloseSession(session_handle);
+              end; //end of for loop
+            end;
+            freemem(SlotIds);
+          end else //no slots found
+            if (slot_count=0) then writeln('no slots found');
+
+          if (Result=CKR_OK) then Result:=pFunctions^.C_Finalize(nil)
+                             else pFunctions^.C_Finalize(nil);
+        end; //C_Initialize failed
+      end else //CK_C_GetFunctionList failed
+      begin
+        Result:=CKR_GENERAL_ERROR;
+        writeln(Format('error 0x%.8x C_GetFunctionList',[Result]));
+      end;
+    end else  //GetProcAddress failed
+    begin
+      Result:=CKR_GENERAL_ERROR;
+    end;
+    FreeLibrary(pkcs11Handle);
+  end else //LoadLibrary failed
+  begin
+    Result:=CKR_GENERAL_ERROR;
+    writeln(PKCS11DLL+' not found');
+    err:=GetLastError;
+    writeln(Format('err is 0x%.8x',[err]));
+  end;
+end;
+
+var
+  retval:CK_ULONG;
+begin
+  retval:=beidsdk_Main;
+
+  write('Done. Return value: '+IntToStr(retval)+' (');
+  if retval=CKR_OK then write('ok') else write('NOT ok');
+  writeln(')');
+  writeln('press a key to exit...');
+  
+  readkey;
+end.
---
+++ cert_registration.pas
@@ -0,0 +1,381 @@
+unit cert_registration;
+
+interface
+
+uses
+  //PKCS11T.pas can be found here :
+  //http://sourceforge.net/p/projectjedi/website/HEAD/tree/trunk/delphi-jedi.org/www/files/API_Not_Assessed/SmartcardRSA/
+  PKCS11T;
+
+function ImportCertificate(certData:CK_BYTE_PTR; certSize:cardinal; cardSerialNumber:CK_BYTE_PTR; cardSerialNumberLen:CK_ULONG):integer;
+
+implementation
+
+uses
+  Windows,sysutils,
+  //wcrypt2.pas can be found here :
+  //http://sourceforge.net/p/projectjedi/website/HEAD/tree/trunk/delphi-jedi.org/www/files/api/CryptoAPI2.zip?format=raw
+  //http://www.delphi-jedi.org/apilibrary.html
+  wcrypt2;
+  //
+
+type
+  //size_t is also defined in delphi XE
+  //C:\Program Files (x86)\Embarcadero\RAD Studio\10.0\source\rtl\posix\Posix.SysTypes.pas
+  {$IFDEF WIN64}
+  size_t = Int64;
+  {$ELSE}
+  size_t = integer;
+  {$ENDIF}
+  //Pascal pointers management
+  CK_BYTES = array[0..65535] of CK_BYTE;
+  CK_BYTES_PTR = ^CK_BYTES;
+  CK_CHAR_PTR_PTR = ^CK_CHAR_PTR;
+
+const
+  CRYPT_E_NOT_FOUND=$80092004;  //http://sourceforge.net/p/mingw/mingw-org-wsl/ci/master/tree/include/winerror.h
+  CERT_FIND_EXISTING=851968;    //http://sourceforge.net/p/mingw/mingw-org-wsl/ci/master/tree/include/wincrypt.h
+                                // CERT_FIND_EXISTING defined in Microsoft wincrypt.h :
+                                //  CERT_COMPARE_EXISTING=13;
+                                //  CERT_COMPARE_SHIFT=16;
+                                //  CERT_FIND_EXISTING=CERT_COMPARE_EXISTING shl CERT_COMPARE_SHIFT;
+  CERT_STORE_ADD_NEWER=6;       //http://sourceforge.net/p/mingw/mingw-org-wsl/ci/master/tree/include/wincrypt.h
+
+function ToHex(uc:CK_BYTE):char;
+begin
+  if uc<=9 then Result:=Chr(48+uc)
+           else Result:=Chr(55+uc);
+end;
+
+function ByteArrayToString(byteArray:CK_BYTES_PTR; ulArrayLen:CK_ULONG):AnsiString;
+var
+  ulOffset:cardinal;
+  i:CK_ULONG;
+begin
+  setlength(Result,ulArrayLen*2+1);
+  ulOffset:=1;  
+  for i:=0 to ulArrayLen-1 do
+  begin
+    Result[ulOffset]:=ToHex(CK_BYTES_PTR(byteArray)^[i] div 16);
+    inc(ulOffset);
+    Result[ulOffset]:=ToHex(CK_BYTES_PTR(byteArray)^[i] mod 16);
+    inc(ulOffset);
+  end;
+  Result[ulOffset]:=#0;
+end;
+
+
+//**************************************************
+// Use Minidriver if OS is Vista or later
+//**************************************************
+function UseMinidriver:boolean;
+var
+  osvi:OSVERSIONINFO;
+begin
+  osvi.dwOSVersionInfoSize:=sizeof(OSVERSIONINFO);
+  GetVersionEx(osvi);
+  Result:=(osvi.dwMajorVersion >= 6);  //Vista or later
+end;
+
+//**************************************************
+// Checks of older registered certificates are not
+// still bound to the CSP when the minidriver is used
+//**************************************************
+function ProviderNameCorrect(pCertContext:PCCERT_CONTEXT):boolean;
+var
+  dwPropId:cardinal;
+  cbData:cardinal;
+  pCryptKeyProvInfo:PCRYPT_KEY_PROV_INFO;
+begin
+  Result:=true;
+  dwPropId:=CERT_KEY_PROV_INFO_PROP_ID;
+  cbData:=0;
+
+  if not UseMinidriver then exit;
+
+  if not CertGetCertificateContextProperty(
+      pCertContext,   // A pointer to the certificate where the property will be set.
+      dwPropId,       // An identifier of the property to get.
+      nil,            // NULL on the first call to get the length.
+      @cbData)        // The number of bytes that must be allocated for the structure.
+  then
+    if GetLastError<>CRYPT_E_NOT_FOUND then
+    begin
+      Result:=false;  // The certificate does not have the specified property.
+      exit;
+    end;
+
+  getmem(pCryptKeyProvInfo,cbData);
+  if CertGetCertificateContextProperty(pCertContext, dwPropId, pCryptKeyProvInfo, @cbData) then
+  begin
+    if pCryptKeyProvInfo^.pwszProvName<>'Belgium Identity Card CSP'
+    then
+      result:=false;
+  end;
+end;
+
+function StoreAuthorityCert(pCertContext:PCCERT_CONTEXT; KeyUsageBits:byte):integer;
+var
+  hMemoryStore:HCERTSTORE;
+  pDesiredCert:PCCERT_CONTEXT;
+begin
+  Result:=0;
+  hMemoryStore:=nil;
+  pDesiredCert:=nil;
+
+  if CompareMem(pCertContext^.pCertInfo^.Issuer.pbData,pCertContext^.pCertInfo^.Subject.pbData,pCertContext^.pCertInfo^.Subject.cbData)
+  then
+    hMemoryStore:=CertOpenSystemStore(0,WideString('ROOT'))
+  else
+    hMemoryStore:=CertOpenSystemStore(0,WideString('CA'));
+
+  if hMemoryStore=nil then
+  begin
+    Result:=GetLastError;
+    writeln('StoreAuthorityCerts: Unable to open the system certificate store. Error code: '+IntToStr(Result)+'.');
+    exit;
+  end;
+
+  pDesiredCert:=CertFindCertificateInStore(
+                hMemoryStore,
+		X509_ASN_ENCODING,
+		0,
+		CERT_FIND_EXISTING,
+		pCertContext,
+		nil);
+  if pDesiredCert<>nil
+  then
+    CertFreeCertificateContext(pDesiredCert)
+  else
+  if GetLastError<>0 then  //pDesiredCert=nil
+  begin
+    CertAddEnhancedKeyUsageIdentifier(pCertContext,szOID_PKIX_KP_EMAIL_PROTECTION);
+    CertAddEnhancedKeyUsageIdentifier(pCertContext,szOID_PKIX_KP_SERVER_AUTH);
+    if CertAddCertificateContextToStore(hMemoryStore,pCertContext,CERT_STORE_ADD_NEWER,pDesiredCert) then
+    begin
+      writeln('StoreAuthorityCerts: Certificate context added to store.');
+      Result:=0;
+    end	else
+    begin
+      Result:=GetLastError;
+      writeln('StoreAuthorityCerts: Unable to add certificate context to store. Error code: '+IntToStr(Result)+'.');
+    end;
+    CertCloseStore(hMemoryStore,CERT_CLOSE_STORE_FORCE_FLAG);
+  end;
+end;
+
+function StoreUserCert(pCertContext:PCCERT_CONTEXT; KeyUsageBits:byte; cardSerialNumber:CK_BYTE_PTR; cardSerialNumberLen:CK_ULONG):integer;
+var
+  hMyStore:HCERTSTORE;
+  pDesiredCert:PCCERT_CONTEXT;
+  pPrevCert:PCCERT_CONTEXT;
+  ContainerName:WideString;
+  //ContainerNameCharLen:size_t;
+  cardSerialNrString:AnsiString;
+  cryptKeyProvInfo:CRYPT_KEY_PROV_INFO;
+  dwPropId:Cardinal;
+  dwFlags:Cardinal;
+
+  // Set friendly names for the certificates
+  //dwsize:longword;
+  //pname:PwideChar;  //http://stackoverflow.com/questions/21867113/how-to-use-unicode-from-delphi-in-c-dll
+  //tpFriendlyName:CRYPT_DATA_BLOB;
+begin
+  Result:=0;
+  pDesiredCert:=nil;
+  pPrevCert:=nil;
+
+  dwPropId:=CERT_KEY_PROV_INFO_PROP_ID;
+  dwFlags:=CERT_STORE_NO_CRYPT_RELEASE_FLAG;
+
+  hMyStore:=CertOpenSystemStore(0,WideString('MY'));
+  if hMyStore=nil then
+  begin
+    Result:=GetLastError;
+    writeln('StoreUserCerts: Unable to open the system certificate store. Error code: '+IntToStr(Result)+'.');
+    exit;
+  end;
+
+  // ----------------------------------------------------
+  // look if we already have a certificate with the same
+  // subject (contains name and NNR) in the store
+  // If the certificate is not found --> nil
+  // ----------------------------------------------------
+  repeat
+    pDesiredCert:=CertFindCertificateInStore(hMyStore, X509_ASN_ENCODING, 0, CERT_FIND_SUBJECT_NAME, @pCertContext^.pCertInfo^.Subject, pPrevCert);
+    if pDesiredCert<>nil then
+    begin
+      // ----------------------------------------------------
+      // If the certificates are identical and function
+      // succeeds, the return value is nonzero, or TRUE.
+      // ----------------------------------------------------
+      if (not CertCompareCertificate(X509_ASN_ENCODING,pCertContext^.pCertInfo,pDesiredCert^.pCertInfo)) or
+         (not ProviderNameCorrect(pDesiredCert)) then
+      begin
+        // ----------------------------------------------------
+        // certificates are not identical, but have the same
+        // subject (contains name and NNR),
+        // so we remove the one that was already in the store
+        // ----------------------------------------------------
+        if not CertDeleteCertificateFromStore(pDesiredCert) then
+          if (GetLastError=Cardinal(E_ACCESSDENIED)) then continue;
+
+	pPrevCert:=nil;
+        continue;
+      end;
+    end;
+    pPrevCert:=pDesiredCert;
+  until pDesiredCert=nil;
+
+  // ----------------------------------------------------
+  // look if we already have the certificate in the store
+  // If the certificate is not found --> NULL
+  // ----------------------------------------------------
+  pDesiredCert:=CertFindCertificateInStore(hMyStore, X509_ASN_ENCODING, 0, CERT_FIND_EXISTING, pCertContext, nil);
+  if pDesiredCert<>nil then
+  begin
+    // ----------------------------------------------------
+    // certificate is already in the store, then just return
+    // ----------------------------------------------------
+    CertFreeCertificateContext(pDesiredCert);
+    CertCloseStore(hMyStore, CERT_CLOSE_STORE_FORCE_FLAG);
+    Result:=0;
+    exit;
+  end;
+
+  cardSerialNrString:=ByteArrayToString(pointer(cardSerialNumber),cardSerialNumberLen);
+
+  if UseMinidriver then
+  begin
+    if (KeyUsageBits and CERT_NON_REPUDIATION_KEY_USAGE)=CERT_NON_REPUDIATION_KEY_USAGE
+    then
+      ContainerName:='NR_'+cardSerialNrString
+    else
+      ContainerName:='DS_'+cardSerialNrString;
+
+    cryptKeyProvInfo.pwszProvName:=WideString('Microsoft Base Smart Card Crypto Provider');
+    cryptKeyProvInfo.dwKeySpec:=AT_SIGNATURE;
+  end else
+  begin
+    if (KeyUsageBits and CERT_NON_REPUDIATION_KEY_USAGE)=CERT_NON_REPUDIATION_KEY_USAGE
+    then
+      ContainerName:='Signature('+cardSerialNrString+')'
+    else
+      ContainerName:='Authentication('+cardSerialNrString+')';
+
+    cryptKeyProvInfo.pwszProvName:=WideString('Belgium Identity Card CSP');
+    cryptKeyProvInfo.dwKeySpec:=AT_KEYEXCHANGE;
+  end;
+  cryptKeyProvInfo.pwszContainerName:=PwideChar(ContainerName);
+
+  cryptKeyProvInfo.dwProvType:=PROV_RSA_FULL;
+  cryptKeyProvInfo.dwFlags:=0;
+  cryptKeyProvInfo.cProvParam:=0;
+  cryptKeyProvInfo.rgProvParam:=nil;
+
+(*
+  // Set friendly names for the certificates
+  dwsize:=CertGetNameStringW(pCertContext, CERT_NAME_ATTR_TYPE, 0, PAnsiChar(szOID_COMMON_NAME), nil, dwsize);
+  getmem(pname,dwsize*sizeof(widechar));
+
+  dwsize:=CertGetNameStringW(pCertContext, CERT_NAME_ATTR_TYPE, 0, PAnsiChar(szOID_COMMON_NAME), pname, dwsize);
+
+  tpFriendlyName.pbData:=PBYTE(pname);
+  tpFriendlyName.cbData:=dwsize*sizeof(WideChar);
+
+  if CertSetCertificateContextProperty(
+                      pCertContext,               // A pointer to the certificate
+                      // where the propertiy will be set.
+                      CERT_FRIENDLY_NAME_PROP_ID, // An identifier of the property to be set.
+                      // In this case, CERT_KEY_PROV_INFO_PROP_ID
+                      // is to be set to provide a pointer with the
+                      // certificate to its associated private key
+                      // container.
+                      dwFlags,                    // The flag used in this case is
+                      // CERT_STORE_NO_CRYPT_RELEASE_FLAG
+                      // indicating that the cryptographic
+                      // context aquired should not
+                      // be released when the function finishes.
+                      @tpFriendlyName            // A pointer to a data structure that holds
+                      // infomation on the private key container to
+                      // be associated with this certificate.
+                      ) then
+*)
+  // Set the property.
+  if CertSetCertificateContextProperty(
+    pCertContext,     // A pointer to the certificate where the property will be set.
+    dwPropId,         // An identifier of the property to be set.
+		      // In this case, CERT_KEY_PROV_INFO_PROP_ID is to be set to provide
+                      // a pointer with the certificate to its associated private key container.
+    dwFlags,          // The flag used in this case is
+		      // CERT_STORE_NO_CRYPT_RELEASE_FLAG indicating that the cryptographic context
+                      // acquired should not be released when the function finishes.
+    @cryptKeyProvInfo // A pointer to a data structure that holds infomation on
+                      // the private key container to be associated with this certificate.
+    ) then
+  begin
+    if (KeyUsageBits and CERT_NON_REPUDIATION_KEY_USAGE)=CERT_NON_REPUDIATION_KEY_USAGE then
+    begin
+      CertAddEnhancedKeyUsageIdentifier(pCertContext, szOID_PKIX_KP_EMAIL_PROTECTION);
+    end else
+    begin
+      CertAddEnhancedKeyUsageIdentifier(pCertContext, szOID_PKIX_KP_EMAIL_PROTECTION);
+      CertAddEnhancedKeyUsageIdentifier(pCertContext, szOID_PKIX_KP_CLIENT_AUTH);
+    end;
+    assert(pDesiredCert=nil);  
+    if CertAddCertificateContextToStore(hMyStore, pCertContext, CERT_STORE_ADD_REPLACE_EXISTING, pDesiredCert) then  //here pDesiredCert=nil
+    begin
+      writeln('StoreUserCerts: Certificate context added to store.');
+      Result:=0;
+    end	else
+    begin
+      Result:=GetLastError;
+      writeln('StoreUserCerts: Unable to add certificate context to store. Error code: '+IntToStr(Result)+'.');
+    end;
+    CertCloseStore(hMyStore, CERT_CLOSE_STORE_FORCE_FLAG);
+    hMyStore:=nil;
+  end;
+  (*
+  freemem(pname);
+  *)
+end;
+
+function ImportCertificate(certData:CK_BYTE_PTR; certSize:cardinal; cardSerialNumber:CK_BYTE_PTR; cardSerialNumberLen:CK_ULONG):integer;
+var
+  pCertContext:PCCERT_CONTEXT;
+  KeyUsageBits:byte;    // Intended key usage bits copied to here.
+begin
+  Result:=0;
+  // ------------------------------------------------------------
+  // create the certificate context with the certificate raw data
+  // ------------------------------------------------------------
+  pCertContext:=CertCreateCertificateContext(X509_ASN_ENCODING or PKCS_7_ASN_ENCODING, pointer(certData),certSize);
+
+  if pCertContext=nil then
+  begin
+    Result:=GetLastError;
+    if Result=E_INVALIDARG
+    then
+      writeln('ImportCertificates: Unable to create certificate context. The certificate encoding type is not supported. Error code: '+IntToStr(Result)+'.')
+    else
+      writeln('ImportCertificates: Unable to create certificate context. Error code: '+IntToStr(Result)+'.');
+  end else
+  begin
+    KeyUsageBits:=0;      // Intended key usage bits copied to here.
+    CertGetIntendedKeyUsage(X509_ASN_ENCODING, pCertContext.pCertInfo,@KeyUsageBits, 1);
+    // ----------------------------------------------------------------------
+    // Only store the context of the certificates with usages for an end-user
+    // i.e. no CA or root certificates
+    // ----------------------------------------------------------------------
+    if((KeyUsageBits and CERT_KEY_CERT_SIGN_KEY_USAGE)=CERT_KEY_CERT_SIGN_KEY_USAGE)
+    then
+      Result:=StoreAuthorityCert(pCertContext, KeyUsageBits)
+    else
+      Result:=StoreUserCert(pCertContext, KeyUsageBits, cardSerialNumber, cardSerialNumberLen);
+
+    if pCertContext<>nil then CertFreeCertificateContext(pCertContext);
+  end;
+end;
+
+
+end.
